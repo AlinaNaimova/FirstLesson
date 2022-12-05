d580680e44c9ac6f7c4846a6e020b4b5b42561ab
Alina N <my_work_space@bk.ru>
2022-12-06 01:37:06 +0300

    finished task 15

--- task15/Program.cs
+++ task15/Program.cs
@@ -5,3 +5,16 @@
 7 -> да
 1 -> нет
 */
+Console.WriteLine("Введите число: "); 
+int num = Convert.ToInt16(Console.ReadLine()); 
+switch (num) 
+{ 
+    case 1: Console.WriteLine("нет"); break; 
+    case 2: Console.WriteLine("нет"); break; 
+    case 3: Console.WriteLine("нет"); break; 
+    case 4: Console.WriteLine("нет"); break; 
+    case 5: Console.WriteLine("нет"); break; 
+    case 6: Console.WriteLine("да"); break; 
+    case 7: Console.WriteLine("да"); break; 
+    default: Console.WriteLine("Ошибка. Введите число от 1 до 7!!!"); break; 
+}
