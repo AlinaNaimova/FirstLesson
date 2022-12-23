9a3db9981a4a972b6254fa5663cff660260e81e2
Alina N <my_work_space@bk.ru>
2022-12-23 12:06:52 +0300

    add task 52 average of column

---
+++ task52/Program.cs
@@ -0,0 +1,76 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+int row = GetNumber("Введите количество строк:");
+int column = GetNumber("Введите количество столбцов:");
+int[,] numbers = new int[row, column];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+AverageNumOfColumn(numbers);
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+void FillArrayRandomNumbers(int[,] numbers)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write(numbers[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+void AverageNumOfColumn(int[,] numbers)
+{
+    double average = 0;
+    Console.Write($"Среднее арифметическое каждого столбца: ");
+    for (int i = 0; i < numbers.GetLength(1); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(0); j++)
+        {
+            average = average + numbers[j, i];
+        }
+        average = average / row;
+        Console.Write(average + "; ");
+    }
+}
