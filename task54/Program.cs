45d095f5af3d7f8422a3231b83e873420b0d89f7
Alina N <my_work_space@bk.ru>
2022-12-28 14:47:24 +0300

    add task 54 sort elements

---
+++ task54/Program.cs
@@ -0,0 +1,86 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+int row = GetNumber("Введите количество строк:");
+int column = GetNumber("Введите количество столбцов:");
+int[,] numbers = new int[row, column];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+SortElements(numbers);
+Console.WriteLine("Массив с упорядоченными элементами:");
+PrintArray(numbers);
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+void FillArrayRandomNumbers(int[,] numbers)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write(numbers[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+void SortElements(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+         for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+        {
+             for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+            {
+                if (numbers[i, z] < numbers[i, z + 1])
+                {
+                    int element = 0;
+                    element = numbers[i, z];
+                    numbers[i, z] = numbers[i, z + 1];
+                    numbers[i, z + 1] = element;
+                }
+            }
+        }
+    }
+Console.WriteLine();
+}
---
+++ task56/Program.cs
@@ -0,0 +1,86 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+int rows = GetNumber("Введите количество строк:");
+int columns = GetNumber("Введите количество столбцов:");
+int[,] numbers = new int[rows, columns];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int minSum = Int32.MaxValue;
+int indexRow = 0;
+
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            sum = sum + numbers[i, j];        
+        }
+        if (sum < minSum)
+        {
+            minSum = sum;
+            indexRow++;
+        }
+    }
+
+Console.WriteLine("Строка с наименьшей суммой елементов: " + (indexRow) + ", а сумма елементов равна: " + (minSum));
+
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+void FillArrayRandomNumbers(int[,] numbers)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write(numbers[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
